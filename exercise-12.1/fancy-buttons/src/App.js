3a56f1e947e05387ea3c7b70267b4af9b426c2fb
josanjohnata <josanjohnata@gmail.com>
2021-05-27 16:00:05 -0300

    Adiciona atividade de fixaçao 4 e 5

--- exercise-12.1/fancy-buttons/src/App.js
+++ exercise-12.1/fancy-buttons/src/App.js
@@ -2,25 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function buttonJoin() {
-  console.log('Você acabou de entar!');
-}
-
-function buttonOut() {
-  console.log('Você acabou de sair!');
-}
-
-function buttonPrint() {
-  console.log('Você acabou de imprimir!');
-}
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.buttonJoin = this.buttonJoin.bind(this);
+    this.buttonOut = this.buttonOut.bind(this);
+    this.buttonPrint = this.buttonPrint.bind(this);
+
+  }
+
+  buttonJoin() {
+    console.log(this)
+    console.log('Você acabou de entar!');
+  }
+  
+  buttonOut() {
+    console.log(this)
+    console.log('Você acabou de sair!');
+  }
+  
+  buttonPrint() {
+    console.log(this)
+    console.log('Você acabou de imprimir!');
+  }
+
   render() {
     return (
       <div>
-        <button onClick={buttonJoin}>Entar</button>
-        <button onClick={buttonPrint}>Imprimir</button>
-        <button onClick={buttonOut}>Sair</button>
+        <button onClick={this.buttonJoin}>Entar</button>
+        <button onClick={this.buttonPrint}>Imprimir</button>
+        <button onClick={this.buttonOut}>Sair</button>
       </div>
     )
   }
