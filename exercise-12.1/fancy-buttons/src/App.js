3d65046f2a3086d3c757f3a2e50b399d6d574522
josanjohnata <josanjohnata@gmail.com>
2021-05-27 19:04:10 -0300

    Adiciona atividade de fixaçao 6 e 7

--- exercise-12.1/fancy-buttons/src/App.js
+++ exercise-12.1/fancy-buttons/src/App.js
@@ -6,33 +6,23 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      clicks: 0
+    }
     this.buttonJoin = this.buttonJoin.bind(this);
-    this.buttonOut = this.buttonOut.bind(this);
-    this.buttonPrint = this.buttonPrint.bind(this);
-
+    
   }
 
   buttonJoin() {
-    console.log(this)
-    console.log('Você acabou de entar!');
-  }
-  
-  buttonOut() {
-    console.log(this)
-    console.log('Você acabou de sair!');
-  }
-  
-  buttonPrint() {
-    console.log(this)
-    console.log('Você acabou de imprimir!');
+    this.setState((estadoAnterior) => ({
+      clicks: estadoAnterior.clicks + 1
+    }))
   }
 
   render() {
     return (
-      <div>
-        <button onClick={this.buttonJoin}>Entar</button>
-        <button onClick={this.buttonPrint}>Imprimir</button>
-        <button onClick={this.buttonOut}>Sair</button>
+      <div className="App">
+        <button onClick={this.buttonJoin}>{this.state.clicks}</button>
       </div>
     )
   }
