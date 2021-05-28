9a17084430c2da0d8759433257cb024e1d7b452e
josanjohnata <josanjohnata@gmail.com>
2021-05-28 15:29:30 -0300

    Inicia os exercicios do bloco 12 dia 2

---
+++ exercise-12.2/my-form-2.0/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <fieldset></fieldset>
+      </form>
+    </div>
+  );
+}
+
+export default App;
