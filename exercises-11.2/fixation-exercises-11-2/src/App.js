0a17a3e5b1c632ace1366ef9902e6f428b096308
josanjohnata <josanjohnata@gmail.com>
2021-06-06 19:07:37 -0300

    finalizando mais um exercicios de fixaçao

--- exercises-11.2/fixation-exercises-11-2/src/App.js
+++ exercises-11.2/fixation-exercises-11-2/src/App.js
@@ -1,43 +1,35 @@
 import React from 'react';
-import Album from './Album';
+import './App.css';
+import Order from './Order';
 
 class App extends React.Component {
   render() {
-    const album01 = {
-      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
-      title: 'Mylo Xyloto',
-      releaseDate: {
-        year: '2011',
-        month: '10',
-        day: '24',
-      },
-      others: {
-        recordCompany: 'Capitol, Parlophone',
-        formats: 'CD, digital'
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
       }
     };
 
-    // Declaração do objeto contendo informações do album02
-    const album02 = {
-      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
-      title: 'Ghost Stories',
-      releaseDate: {
-        year: '2014',
-        month: '05',
-        day: '16',
-      },
-      others: {
-        recordCompany: 'Parlophone',
-        formats: 'CD, digital'
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
       }
     };
 
-    // Retorno do que será renderizado
     return (
-      <div>
-        <Album album={ album01 } />
-        <Album album={ album02 } />
-    </div>
+      <div className="App">
+        <h1> Orders recently created </h1>
+        <Order order={headphone} />
+        <Order order={energyDrink} />
+      </div>
     );
   }
 }
